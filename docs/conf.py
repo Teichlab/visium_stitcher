dad3fd9e511280d9a5eba20fe5712cd75e7e2c04
ktpolanski <krzysztof_polanski@o2.pl>
2023-03-27 15:09:17 +0100

    docs

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+autodoc_mock_imports = ['matplotlib.image', 'scipy.spatial', 'xmltodict', 'anndata', 'numpy', 'cv2']
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Visium stitcher'
+copyright = '2023, Nadav Yayon, Krzysztof Polanski, Valentina Lorenzi'
+author = 'Nadav Yayon, Krzysztof Polanski, Valentina Lorenzi'
+release = '0.0.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,10 @@
+.. Visium_stitcher documentation master file, created by
+   sphinx-quickstart on Thu Feb  2 10:21:06 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Visium stitcher's documentation!
+=====================================
+
+.. automodule:: visium_stitcher
+   :members:  stitch, transform_finder
